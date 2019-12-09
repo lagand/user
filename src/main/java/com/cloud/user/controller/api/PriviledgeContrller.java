00bae2c057dbdaa3ae53e26e2694dc7e7f6114a1
luyi <519533392@qq.com>
2019-12-09 23:12:16 +0800

    Initial commit

---
+++ src/main/java/com/cloud/user/controller/api/PriviledgeContrller.java
@@ -0,0 +1,20 @@
+package com.cloud.user.controller.api;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PriviledgeContrller {
+
+    private static final Logger logger = LoggerFactory.getLogger(PriviledgeContrller.class);
+
+    @RequestMapping("/getUser")
+    public String getUser(@RequestParam(value="name") String name){
+        logger.info(" recived user name : {}", name);
+        return "user"+name;
+    }
+}
